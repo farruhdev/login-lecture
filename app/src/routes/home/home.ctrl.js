1f87929e115796de1a71d0222eee92951b78b769
Farrukh <farrukh1994@naver.com>
2023-11-06 02:33:17 +0900

    풀터 구조 최 적화

---
+++ app/src/routes/home/home.ctrl.js
@@ -0,0 +1,13 @@
+"use strict"; 
+const hello = (req, res) => {
+    res.render("home/index");
+};
+
+const login = (req,res) => {
+    res.render("home/login");
+};
+
+module.exports = {
+    hello,
+    login,
+};
